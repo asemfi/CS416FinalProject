ae7fc753e88c746cf6029e6be8ef0b48537b82cf
asemfi <fuadibnulamin@gmail.com>
2023-11-22 05:58:00 -0500

    added urls.py in ticketmaster app and created corresponding url

--- ticketMaster/views.py
+++ ticketMaster/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+def ticketmaster(request):
+    return HttpResponse("Hello Ticket Master")
