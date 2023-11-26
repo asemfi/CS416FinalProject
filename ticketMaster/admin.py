f0c022b6211549e8e58f772e75693abb67c1010e
jcgartley <jocelyn.gartley@gmail.com>
2023-11-26 13:56:24 -0500

    built db bones Jocelyn

--- ticketMaster/admin.py
+++ ticketMaster/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from ticketMaster.models import Event
+
 # Register your models here.
+admin.site.register(Event)
--- ticketMaster/models.py
+++ ticketMaster/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    eventID = models.CharField(max_length=20)
+    eventName = models.CharField(max_length=200)
+    localDateTime = models.DateTimeField()
+    venue = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    imageLink = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.eventID
+
+
+class Comments(models.Model):
+    eventID = models.ForeignKey(Event, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200)
+    comment = models.CharField(max_length=2000)
