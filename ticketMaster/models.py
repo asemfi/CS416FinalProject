2b629a078f335add3fc4777bfc516a94324c32c2
jcgartley <jocelyn.gartley@gmail.com>
2023-11-28 16:30:19 -0500

    fixed user field in savedevents and comments migrated db Jocelyn
    (cherry picked from commit cff053d1a2fe6fd43feeb343f084ab552acff894)

--- ticketMaster/models.py
+++ ticketMaster/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # User is an object that correlates to rows of the auth_user table
 
+# User is an object that correlates to rows of the auth_user table
+
 # Create your models here.
