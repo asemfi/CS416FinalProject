e4d4b2fa9b527282799bc0b6362eee276ba03e88
jcgartley <jocelyn.gartley@gmail.com>
2023-11-28 11:17:34 -0500

    added star rating field to comment db eventID must be unique Jocelyn

--- ticketMaster/models.py
+++ ticketMaster/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    eventID = models.CharField(max_length=20)
+    eventID = models.CharField(max_length=20, unique=True)
     eventName = models.CharField(max_length=200)
     localDateTime = models.DateTimeField()
     venue = models.CharField(max_length=200)
@@ -18,4 +18,5 @@ class Event(models.Model):
 class Comments(models.Model):
     eventID = models.ForeignKey(Event, on_delete=models.CASCADE)
     username = models.CharField(max_length=200)
+    starRating = models.IntegerField()
     comment = models.CharField(max_length=2000)
