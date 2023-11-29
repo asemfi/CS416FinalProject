87687211a50017082e88f9db8a6ceab589d0eb78
asemfi <fuadibnulamin@gmail.com>
2023-11-29 05:52:33 -0500

    handles no search results exception

--- ticketMaster/views.py
+++ ticketMaster/views.py
@@ -38,76 +38,86 @@ def ticketmaster(request):
         else:
             print("Else Block")
             # print the response for testing purpose (open "Run" at the bottom to see what is printed)
+            if '_embedded' in event_search_result and event_search_result['_embedded']:
+                events = event_search_result['_embedded']['events']
 
-            # Store each user's information in a variable
-            events = event_search_result['_embedded']['events']
+                # Initialize an empty list to store user data
+                event_list = []
 
+                # Iterate through each user in the 'events' list coming from the api
+                # Rather than directly passing the "events" array to the template,
+                # the following approach allows server-side processing and formatting of specific data (e.g., date).
+                # So, the template only needs to plug in the preprocessed information.
+                for item in events:
+                    event = {}
+                    event_name = item['name']
+                    event_link = item['url']
+                    event_img_url = item['images'][1]['url']
+                    # Adding error handling for potential missing keys
+                    if 'dates' in item and 'start' in item['dates'] and 'dateTime' in item['dates']['start']:
+                        event_date = item['dates']['start']['dateTime']
+                        # Convert the string to a datetime object
+                        date_object = datetime.strptime(event_date, "%Y-%m-%dT%H:%M:%SZ")
 
+                        # Format the datetime object into the desired readable format
+                        formatted_date = date_object.strftime("%a %b %d %Y")
 
-            # Initialize an empty list to store user data
-            event_list = []
+                        event_time = item['dates']['start']['localTime']
 
-            # Iterate through each user in the 'events' list coming from the api
-            # Rather than directly passing the "events" array to the template,
-            # the following approach allows server-side processing and formatting of specific data (e.g., date).
-            # So, the template only needs to plug in the preprocessed information.
-            for item in events:
-                event = {}
-                event_name = item['name']
-                event_link = item['url']
-                event_img_url = item['images'][1]['url']
-                # Adding error handling for potential missing keys
-                if 'dates' in item and 'start' in item['dates'] and 'dateTime' in item['dates']['start']:
-                    event_date = item['dates']['start']['dateTime']
-                    # Convert the string to a datetime object
-                    date_object = datetime.strptime(event_date, "%Y-%m-%dT%H:%M:%SZ")
-
-                    # Format the datetime object into the desired readable format
-                    formatted_date = date_object.strftime("%a %b %d %Y")
-
-                    event_time = item['dates']['start']['localTime']
-
-                    # Check for the presence of time data and format it
-                    if event_time:
-                        try:
-                            time_obj = datetime.strptime(event_time,
-                                                         "%H:%M:%S")  # Assuming time is in HH:MM:SS format
-                            formatted_time = time_obj.strftime("%I:%M %p")  # Format time as desired
-                            event_time = formatted_time
-                        except ValueError:
-                            # Handle cases where the time format is unexpected
-                            event_time = 'N/A'
+                        # Check for the presence of time data and format it
+                        if event_time:
+                            try:
+                                time_obj = datetime.strptime(event_time,
+                                                             "%H:%M:%S")  # Assuming time is in HH:MM:SS format
+                                formatted_time = time_obj.strftime("%I:%M %p")  # Format time as desired
+                                event_time = formatted_time
+                            except ValueError:
+                                # Handle cases where the time format is unexpected
+                                event_time = 'N/A'
 
 
-                else:
-                    # Handle missing or unexpected data, set a default value or skip this item
-                    event_date = 'N/A'  # Set a default value
-                    event_time = 'N/A'  # set a default value
+                    else:
+                        # Handle missing or unexpected data, set a default value or skip this item
+                        event_date = 'N/A'  # Set a default value
+                        event_time = 'N/A'  # set a default value
 
-                event_venue_name = item['_embedded']['venues'][0]['name']
-                event_address = item['_embedded']['venues'][0]['address']['line1']
-                event_state = item['_embedded']['venues'][0]['state']['name']
-                event_city = item['_embedded']['venues'][0]['city']['name']
-                event_city_state = event_city + ' , ' + event_state
-                # Create a new dictionary to store event details
-                event_details = {
-                    'event_name': event_name,
-                    'event_link': event_link,
-                    'event_img_url': event_img_url,
-                    'event_venue_name': event_venue_name,
-                    'event_time': formatted_time,
-                    'event_date': formatted_date,
-                    'event_address': event_address,
-                    'event_city_state': event_city_state
+                    event_venue_name = item['_embedded']['venues'][0]['name']
+                    event_address = item['_embedded']['venues'][0]['address']['line1']
+                    event_state = item['_embedded']['venues'][0]['state']['name']
+                    event_city = item['_embedded']['venues'][0]['city']['name']
+                    event_city_state = event_city + ' , ' + event_state
+                    # Create a new dictionary to store event details
+                    event_details = {
+                        'event_name': event_name,
+                        'event_link': event_link,
+                        'event_img_url': event_img_url,
+                        'event_venue_name': event_venue_name,
+                        'event_time': formatted_time,
+                        'event_date': formatted_date,
+                        'event_address': event_address,
+                        'event_city_state': event_city_state
 
-                }
-                event_list.append(event_details)
+                    }
+                    event_list.append(event_details)
 
-            print('printing event_list')
-            print(event_list)
-            # Create a context dictionary with the event_list and render the 'index.html' template
-            context = {'events': event_list}
-            return render(request, 'ticketmaster.html', context)
+                print('printing event_list')
+                print(event_list)
+                # Create a context dictionary with the event_list and render the 'index.html' template
+                context = {'events': event_list}
+                return render(request, 'ticketmaster.html', context)
+
+
+            else:
+                messages.info(request, 'NO RESULT FOUND.')
+                # redirect user to the index page
+                return redirect('ticketmaster')
+
+
+
+
+
+
+
 
     # all other cases, just render the page without sending/passing any context to the template
     print('return without post or get')
