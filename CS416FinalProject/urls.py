9dd31f48c9948d829b6bd388dd0d61725dcf7b32
asemfi <fuadibnulamin@gmail.com>
2023-11-29 19:37:23 -0500

    merging localBranchFuad with master
    (cherry picked from commit d509274f4e9f793570072d43969c8bda4b37daba)

--- CS416FinalProject/urls.py
+++ CS416FinalProject/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ticketmaster/', include('ticketMaster.urls')),
     path('accounts/', include('accounts.urls')),
-
 ]
