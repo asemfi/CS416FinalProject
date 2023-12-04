f8ba7d0142d2e729b738accdf04621773ada3c5e
rananaimat <rananaimat8@gmail.com>
2023-12-04 18:37:52 -0500

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,17 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('register/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
-    path('logout/', views.logout_view, name="logout")
+    path('logout/', views.logout_view, name="logout"),
 
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
