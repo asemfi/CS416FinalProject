48e473d14d9661b98ceb21204e1ede5d5fd14ce0
rananaimat <rananaimat8@gmail.com>
2023-12-05 17:32:05 -0500

    modification

--- CS416FinalProject/urls.py
+++ CS416FinalProject/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ticketmaster/', include('ticketMaster.urls')),
     path('accounts/', include('accounts.urls')),
+    #path('users/', include('users.urls'))
 
 ]
