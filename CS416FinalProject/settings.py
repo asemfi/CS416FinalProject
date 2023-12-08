ceb83d42fc582841643711b07d58f31daae82e01
rananaimat <rananaimat8@gmail.com>
2023-12-08 11:53:38 -0500

    bug fixes passweor forget functionality and edit user functionality added.

--- CS416FinalProject/settings.py
+++ CS416FinalProject/settings.py
@@ -127,10 +127,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_DIRS = ['ticketMaster/static']
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'ticketmasterapi@gmail.com'
-# EMAIL_HOST_PASSWORD = 'MessiGOAT@10'
+STATICFILES_DIRS = ['ticketMaster/static']
+
+
+# Email Related
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = 'ticketmasterapi@gmail.com'
+EMAIL_HOST_USER = 'ticketmasterapi@outlook.com'
+DEFAULT_FROM_EMAIL = 'ticketmasterapi@outlook.com'
+EMAIL_HOST_PASSWORD = 'MessiGOAT@10'
+SERVER_EMAIL = 'ticketmasterapi@outlook.com'
+
--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,8 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
+    bio = forms.CharField(max_length=200)
 
     class Meta:
         model = User
-        fields = ("username", 'first_name', "last_name", 'email', 'password1', 'password2')
+        fields = ("username", 'first_name', "last_name", 'email', 'password1', 'password2', 'bio')
